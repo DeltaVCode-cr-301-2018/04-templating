bfa7bc5b94171a64499267048cac44b78ee9b302
tektechnologies <info@tektechnologies.com>
2018-07-12 15:28:37 -0500

    adds article.js code

--- Taylor-Craig/scripts/article.js
+++ Taylor-Craig/scripts/article.js
@@ -13,6 +13,14 @@ function Article (rawDataObj) {
 
 Article.prototype.toHtml = function() {
   // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  let templateHtml =  $('#article-template').text();
+  let renderTemplate  = Handlebars.compile(templateHtml);
+
+
+  return renderTemplate(this);
+
+
+
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
